ea83f0626824cd1e516df37f9ee4e1a40f675df8
Goga <2doug077@gmail.com>
2022-07-06 13:18:50 -0300

    12

--- javascript.js
+++ javascript.js
@@ -35,3 +35,42 @@ function trocaFundo(){
 
     
 }
+
+function escondesemana(){
+    const seg = document.querySelector('#hide1');
+    const ter = document.querySelector('#hide2');
+    const qua = document.querySelector('#hide3');
+    const sem = document.querySelector('#sem');
+    const fim = document.querySelector('#fim');
+
+    if (seg.classList.contains('hide')){
+        seg.classList.remove('hide')
+        ter.classList.remove('hide')
+        qua.classList.remove('hide')
+        seg.classList.add('nohide')
+        ter.classList.add('nohide')
+        qua.classList.add('nohide')
+        
+        sem.classList.remove('hide')
+        sem.classList.add('nohide')
+        
+        fim.classList.remove('nohide')
+        fim.classList.add('hide')
+    }else{
+        seg.classList.remove('nohide')
+        ter.classList.remove('nohide')
+        qua.classList.remove('nohide')
+        seg.classList.add('hide')
+        ter.classList.add('hide')
+        qua.classList.add('hide')
+
+        sem.classList.remove('nohide')
+        sem.classList.add('hide')
+        
+        fim.classList.remove('hide')
+        fim.classList.add('nohide')
+
+        
+    }
+    
+}
